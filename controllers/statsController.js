97c6c19c10f97d660da9f33c90e752bd2a432fd1
Gal <galsinai33@gmail.com>
2023-05-06 15:08:17 +0300

    fix response of stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -68,21 +68,77 @@ async function addToDonateClicksList(req, res) {
 }
 
 // Function to get the list of entry clicks
+// async function getEntryClicksList(req, res) {
+//   try {
+//     const stats = await Stats.findOne({});
+//     res.json(
+//       stats.entryClicks.aggregate([
+//         {
+//           $group: {
+//             _id: { $dateToString: { format: "%Y-%m-%d", date: "date" } },
+//             count: { $sum: 1 },
+//           },
+//         },
+//         {
+//           $project: {
+//             date: "$_id",
+//             count: 1,
+//             _id: 0,
+//           },
+//         },
+
+//         { $sort: { date: 1 } },
+//       ])
+//     );
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Server Error");
+//   }
+// }
 async function getEntryClicksList(req, res) {
   try {
     const stats = await Stats.findOne({});
-    res.json(stats.entryClicks);
+    const entryClicks = stats.entryClicks;
+    const entryClicksByDate = entryClicks.reduce((acc, click) => {
+      const date = click.date.toISOString().split("T")[0];
+      acc[date] = (acc[date] || 0) + 1;
+      return acc;
+    }, {});
+    const result = Object.keys(entryClicksByDate).map((date) => ({
+      date,
+      count: entryClicksByDate[date],
+    }));
+    res.json(result);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
   }
 }
 
-// Function to get the list of donation clicks
-async function getDonateClicksList(req, res) {
+async function getDonationClicksList(req, res) {
   try {
     const stats = await Stats.findOne({});
-    res.json(stats.donationClicks);
+    const donationClicks = stats.donationClicks;
+    const donateClicksByDate = donationClicks.reduce((acc, click) => {
+      const date = click.date.toISOString().split("T")[0];
+      acc[date] = (acc[date] || 0) + 1;
+      return acc;
+    }, {});
+    const result = Object.keys(donateClicksByDate).map((date) => ({
+      date,
+      count: donateClicksByDate[date],
+    }));
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+}
+
+async function getAllStats(req, res) {
+  try {
+    const stats = await Stats.findOne({});
+    res.json(stats);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
@@ -94,5 +150,6 @@ module.exports = {
   addToEntryClicksList,
   addToDonateClicksList,
   getEntryClicksList,
-  getDonateClicksList,
+  getDonationClicksList,
+  getAllStats,
 };
--- routes/statsRoute.js
+++ routes/statsRoute.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   getClothesByQuantityAndDate,
-  addToDonateClicksList,
-  addToEntryClicksList,
-  getDonateClicksList,
+  getAllStats,
+  getDonationClicksList,
   getEntryClicksList,
 } = require("../controllers/statsController");
 
 router.get("/clothes", getClothesByQuantityAndDate);
-router.get("/stats-donations", getDonateClicksList);
+router.get("/stats-donations", getDonationClicksList);
 router.get("/stats-entries", getEntryClicksList);
+router.get("/", getAllStats);
 module.exports = router;
